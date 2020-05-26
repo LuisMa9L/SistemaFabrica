672e9103ad2b9ceda2dcc3f137c5edb920416d7f
lMMl <soy.lmml@gmail.com>
2020-05-25 19:16:21 -0600

    Adjuntar archivo en correo

--- src/main/java/com/proyecto/fabrica/service/SendMailService.java
+++ src/main/java/com/proyecto/fabrica/service/SendMailService.java
@@ -1,6 +1,9 @@
 package com.proyecto.fabrica.service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -26,7 +29,23 @@ public class SendMailService {
         mail.setText(body);
 
 
-        javaMailSender.send(mail);
+        //javaMailSender.send(mail);
+        
+        MimeMessage message = javaMailSender.createMimeMessage();
+        String cc = "";
+        cc = "Archivo; De ; Prueba;";
+        
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body);
+            helper.addAttachment("MyTestFile.csv", new ByteArrayResource(cc.getBytes()));
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+
+        }
 
 
     }
